c22f1978f1d298da1d22317ac601997d7e5c3a80
iniyan <iniyan455@gmail.com>
2020-09-10 18:04:43 +0530

    init setup

---
+++ README.md
@@ -0,0 +1,11 @@
+## SMS User Consent API
+
+SMS User Consent API gives the content of a single SMS message to us if the user gives permission. 
+For this, a bottom sheet will show on the screen and ask the user for permission access to SMS.
+
+## Sample Usage
+
+I explained step by step usage of SMS User Consent API on my blog post. You can check on it.
+
+
+Ref Understanding  - https://proandroiddev.com/new-way-of-sms-retriever-api-sms-user-consent-d7210c00d7c9
---
+++ app/src/main/java/com/iniyan/androidsmsuserconsentapi/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.iniyan.androidsmsuserconsentapi
+
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var smsBroadcastReceiver: SmsBroadcastReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startSmsUserConsent()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerToSmsBroadcastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(smsBroadcastReceiver)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQ_USER_CONSENT -> {
+                if ((resultCode == Activity.RESULT_OK) && (data != null)) {
+                    //That gives all message to us. We need to get the code from inside with regex
+                    val message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)
+                    val code = message?.let { fetchVerificationCode(it) }
+
+                    etVerificationCode.setText(code)
+                }
+            }
+        }
+    }
+
+    private fun startSmsUserConsent() {
+        SmsRetriever.getClient(this).also {
+            //We can add user phone number or leave it blank
+            it.startSmsUserConsent(null)
+                    .addOnSuccessListener {
+                        Log.d(TAG, "LISTENING_SUCCESS")
+                    }
+                    .addOnFailureListener {
+                        Log.d(TAG, "LISTENING_FAILURE")
+                    }
+        }
+    }
+
+    private fun registerToSmsBroadcastReceiver() {
+        smsBroadcastReceiver = SmsBroadcastReceiver().also {
+            it.smsBroadcastReceiverListener = object : SmsBroadcastReceiver.SmsBroadcastReceiverListener {
+                override fun onSuccess(intent: Intent?) {
+                    intent?.let { context -> startActivityForResult(context, REQ_USER_CONSENT) }
+                }
+
+                override fun onFailure() {
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        registerReceiver(smsBroadcastReceiver, intentFilter)
+    }
+
+    private fun fetchVerificationCode(message: String): String {
+        return Regex("(\\d{6})").find(message)?.value ?: ""
+    }
+
+    companion object {
+        const val TAG = "SMS_USER_CONSENT"
+
+        const val REQ_USER_CONSENT = 100
+    }
+}
