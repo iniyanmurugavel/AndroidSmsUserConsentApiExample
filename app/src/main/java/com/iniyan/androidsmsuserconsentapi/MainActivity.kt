4cab222399a635dc8dd16f80cf0f830d95a7577c
iniyan <iniyan455@gmail.com>
2020-09-11 18:59:55 +0530

    show phoneNumber hint

--- app/src/main/java/com/iniyan/androidsmsuserconsentapi/MainActivity.kt
+++ app/src/main/java/com/iniyan/androidsmsuserconsentapi/MainActivity.kt
@@ -6,18 +6,21 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.credentials.Credential
+import com.google.android.gms.auth.api.credentials.Credentials
+import com.google.android.gms.auth.api.credentials.HintRequest
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val CREDENTIAL_PICKER_REQUEST = 1  // Set to an unused request code
     lateinit var smsBroadcastReceiver: SmsBroadcastReceiver
-
+    lateinit var  mobileNo :String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        startSmsUserConsent()
+        requestHint()
+       // startSmsUserConsent()
     }
 
     override fun onStart() {
@@ -31,7 +34,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
+            CREDENTIAL_PICKER_REQUEST ->
+                // Obtain the phone number from the result
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val credential = data.getParcelableExtra<Credential>(Credential.EXTRA_KEY)
+                    // credential.getId();  <-- will need to process phone number string
+
+                    mobileNo = credential!!.id
+                    Log.i(TAG, "onActivityResult: mobileNo $mobileNo")
+                    startSmsUserConsent()
+                }
             REQ_USER_CONSENT -> {
                 if ((resultCode == Activity.RESULT_OK) && (data != null)) {
                     //That gives all message to us. We need to get the code from inside with regex
@@ -47,13 +61,13 @@ class MainActivity : AppCompatActivity() {
     private fun startSmsUserConsent() {
         SmsRetriever.getClient(this).also {
             //We can add user phone number or leave it blank
-            it.startSmsUserConsent(null)
-                    .addOnSuccessListener {
-                        Log.d(TAG, "LISTENING_SUCCESS")
-                    }
-                    .addOnFailureListener {
-                        Log.d(TAG, "LISTENING_FAILURE")
-                    }
+            it.startSmsUserConsent(null) // we can pass mobileNumber
+                .addOnSuccessListener {
+                    Log.d(TAG, "LISTENING_SUCCESS")
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "LISTENING_FAILURE")
+                }
         }
     }
 
@@ -81,5 +95,20 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "SMS_USER_CONSENT"
 
         const val REQ_USER_CONSENT = 100
+    }
+
+
+    // Construct a request for phone numbers and show the picker
+    private fun requestHint() {
+        val hintRequest = HintRequest.Builder()
+            .setPhoneNumberIdentifierSupported(true)
+            .build()
+        val credentialsClient = Credentials.getClient(this)
+        val intent = credentialsClient.getHintPickerIntent(hintRequest)
+        startIntentSenderForResult(
+            intent.intentSender,
+            CREDENTIAL_PICKER_REQUEST,
+            null, 0, 0, 0
+        )
     }
 }
